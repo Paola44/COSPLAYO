bdca482562ef7384c0aa8c4fd1c52cb8d6366ff7
Paola Carbonnel paolacarbonnel@hotmail.com <paolacarbonnel@hotmail.com>
2019-11-25 18:14:46 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :costumes, only:[:create, :new, :show, :index] do
+    resources :reservations, only:[:new, :create]
+  end
+  namespace :owner do ==>> meme chose avec 'renter'
+    resources :costumes, only: [:index]
+  end
+  namespace :owner do
+    resources :reservations, only: [:index]
+  end
 end
